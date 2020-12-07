0b1b74d2322889bbc247e22ca6ef5920e8e67230
seobokyeong <if0921@daum.net>
2020-12-07 09:09:33 +0900

    포트폴리오

--- js/common.js
+++ js/common.js
@@ -1,12 +1,16 @@
 $(function () {
   // 전체메뉴
-  var hamburger = $('.hamburger')
+  // var hamburger = $('.hamburger')
+  // var close = $('.close')
+  // var menu = $('.menu')
+  // hamburger.click(function () {
+  //   menu.show()
+  // })
+  // close.click(function () {
+  //   menu.hide()
+  // })
   var close = $('.close')
-  var menu = $('.menu')
-  hamburger.click(function () {
-    menu.show()
-  })
   close.click(function () {
-    menu.hide()
+    window.history.back()
   })
 })
