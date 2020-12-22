10ed979e2474c446ab8bdabf8f1d1046e12b1b57
seobokyeong <if0921@daum.net>
2020-12-22 17:50:16 +0900

    포폴

--- js/common.js
+++ js/common.js
@@ -13,4 +13,21 @@ $(function () {
   close.click(function () {
     window.history.back()
   })
+
+  // 헤더 스크립트
+  var goTop = $('.go_top')
+
+  $(window).scroll(function () {
+    var scrollTopValue = $(document).scrollTop()
+
+    if (scrollTopValue > 900) {
+      goTop.fadeIn()
+    } else {
+      goTop.fadeOut()
+    }
+  })
+
+  goTop.click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 400)
+  })
 })
